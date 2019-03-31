c25713b27e56fcaa3f3c6c9c238fe54b90c4fbb0
Monika <monika@whoin.space>
2019-03-31 13:38:23 +0300

    Added PostgresSQL env variable.

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -6,7 +7,7 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 #reqparse for parsing the requests, so not all data has to be read and posted each time
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db') #environment variable, default variabe (if environment failed)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'pupsiusOPx'
 api = Api(app)
