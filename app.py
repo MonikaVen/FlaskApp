87d5015f3bba54bcac41d7ca1fc5b342ee41ef26
Monika <monika@whoin.space>
2019-04-08 20:34:52 +0300

    Added stores and database use with SQLAlchemy

--- app.py
+++ app.py
@@ -2,21 +2,27 @@ import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
+
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
+from resources.store import Store, StoreList
 #reqparse for parsing the requests, so not all data has to be read and posted each time
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db') #environment variable, default variabe (if environment failed)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True # To allow flask propagating exception even if debug is set to false on app
 app.secret_key = 'pupsiusOPx'
 api = Api(app)
-app.config['PROPAGATE_EXCEPTIONS'] = True # To allow flask propagating exception even if debug is set to false on app
+
+@app.before_first_request
+def create_tables():
+	db.create_all()
+
 jwt = JWT(app, authenticate, identity) # creates /auth endpoint
 
-
-
-
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(StoreList, '/stores')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
--- resources/item.py
+++ resources/item.py
@@ -1,4 +1,3 @@
-import sqlite3
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
 from models.item import ItemModel
@@ -9,6 +8,11 @@ class Item(Resource):
 		type=float,
 		required=True,
 		help="This field cannot be left blank!"
+	)
+	parser.add_argument('store_id',
+		type=int,
+		required=True,
+		help="Every item needs a store id!"
 	)
 	@jwt_required()
 	def get(self, name):
@@ -22,7 +26,7 @@ class Item(Resource):
 		if ItemModel.find_by_name(name):
 			return {'message': 'An item with name {} already exists.'.format(name)}, 400 
 		data = Item.parser.parse_args()
-		item = ItemModel(name, data['price'])
+		item = ItemModel(name, data['price'], data['store_id'])
 		try:
 			item.save_to_db()
 		except:
@@ -33,16 +37,17 @@ class Item(Resource):
 		item = ItemModel.find_by_name(name)
 		if item:
 			item.delete_from_db()
-		return {'message': 'Item deleted'}
+			return {'message': 'Item deleted'}
+		return {'message': 'Item not found.'}, 404
 
 	def put(self, name):
 		data = Item.parser.parse_args()
 		item = ItemModel.find_by_name(name)
 
-		if item is None:
-			item = ItemModel(name, data['price'])
-		else:
+		if item:
 			item.price = data['price']
+		else:
+			item = ItemModel(name, data['price'], data['store_id'])	
 		item.save_to_db()
 		return item.json()
 #201 - CREATED
@@ -51,13 +56,4 @@ class Item(Resource):
 
 class ItemList(Resource):
 	def get(self):
-		# connection = sqlite3.connect('data.db')
-		# cursor = connection.cursor()
-		# query = "SELECT * FROM items"
-		# result = cursor.execute(query)
-		# items = []
-		# for row in result:
-		# 	items.append({'name': row[0], 'price': row[1]})
-		# connection.close()
-		# return {'items': items}
-		return {'items': [x.json() for x in ItemModel.find_all()]}
+		return {'items': [item.json() for item in ItemModel.query.all()]}
